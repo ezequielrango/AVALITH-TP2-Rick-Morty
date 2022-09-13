856979a75e0e266068de333e1dbe20fc2415f614
Ezequiel Rango <ezequielrango29@gmail.com>
2022-09-13 10:22:07 -0300

    :fire: create form

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+const Form = () => {
+    const [email, setemail] = useState('');
+    const [username, setUsername] = useState('');
+    console.log('Soy el estado de username :', username);
+    console.log('Soy el estado de email :', email);
+    
+    return(
+        <div className="form">
+        <form 
+        onSubmit={(e) => {
+            e.preventDefault()
+        }}
+        >
+        <h1 className="formTitle">Form</h1>
+            <input 
+                className="input"
+                type='text' 
+                autoComplete="off" 
+                placeholder="Insert here your username"
+                name='email'
+                value={username}
+                onChange={ (e) => setUsername(e.target.value)}          
+            ></input>
+            <input
+                className="input" 
+                type='text'
+                name="username"
+                placeholder="Insert here your email"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+                >
+            </input>
+            <div className="btnContainer">
+            <button     className="btn-submit"  type='submit'>Submit</button>
+
+            </div>
+        </form>
+        </div>
+    )
+}
+
+export default Form;
