db020498bde780516935cfe63d87beec2cce8449
Ezequiel Rango <ezequielrango29@gmail.com>
2022-09-13 10:21:30 -0300

    :construction: : create card Character

---
+++ src/components/Character.js
@@ -0,0 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom'
+
+const Character = ({ name, image, status, specie, id }) => {
+    return ( //aca va la card
+        <div className='cardPrueba'>
+            <div key={id} className="col-lg-4 col-md-6 col-12 mb-3 position-relative " >
+                <div className="cards d-flex flex-column justify-content-center">
+                    <img src={image} className="card-img-top" alt={name} />
+                    <h2 className='card-title'>{name}</h2>
+                    <h5 className="cardText"><span> Especie:</span> {specie}</h5>
+                    <h5 className="cardText"><span> Estado:</span>{status}</h5>
+                    <Link to={`/characters/${id}`}>
+                        <button className='buttonDetail'>Detail</button>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Character;
