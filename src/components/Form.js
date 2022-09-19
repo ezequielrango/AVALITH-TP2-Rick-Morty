de48fa071ad3ca2f8d8357707b4d930fee62ec6e
[kucki90] <[c.kuckiewicz@hotmail.com]>
2022-09-19 17:08:59 -0300

    :lipstick: adding style in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,13 @@ import { useState } from "react"
 
 const Form = () => {
     const [email, setemail] = useState('');
-    const [username, setUsername] = useState('');
-    console.log('Soy el estado de username :', username);
+    const [name, setname] = useState('');
+    const [celphone, setcelphone] = useState('');
+    const [comment, setcomment] = useState('');
+    console.log('Soy el estado de name :', name);
     console.log('Soy el estado de email :', email);
+    console.log('Soy el estado de celphone :', celphone);
+    console.log('Soy el estado de comment :', comment);
     
     return(
         <div className="containerDelContainer">
@@ -14,25 +18,44 @@ const Form = () => {
             e.preventDefault()
         }}
         >
-        <h1 className="formTitle">Form</h1>
+        <h1 className="formTitle">Formulario de contacto</h1>
+        <h5 className="formTitle">Completá el siguiente formulario con tus datos y nos pondremos en contacto a la brevedad</h5>
             <input 
                 className="input"
                 type='text' 
                 autoComplete="off" 
-                placeholder="Insert here your username"
+                placeholder="Nombre"
                 name='email'
-                value={username}
-                onChange={ (e) => setUsername(e.target.value)}          
+                value={name}
+                onChange={ (e) => setname(e.target.value)}          
             ></input>
             <input
                 className="input" 
                 type='text'
                 name="username"
-                placeholder="Insert here your email"
+                placeholder="E-mail de contacto"
                 value={email}
                 onChange={(e) => setemail(e.target.value)}
                 >
             </input>
+            <input
+                className="input" 
+                type='text'
+                name="celphone"
+                placeholder="Teléfono de contacto"
+                value={celphone}
+                onChange={(e) => setcelphone(e.target.value)}
+                >
+            </input>
+            <textarea
+                className="textarea" 
+                type='text'
+                name="textarea"
+                placeholder="Dejanos tu comentario"
+                value={comment}
+                onChange={(e) => setcomment(e.target.value)}
+                >
+            </textarea>
             <div className="btnContainer">
             <button     className="btn-submit"  type='submit'>Submit</button>
 
